8e1f3b664962737fe7470d95e4fd5343ba5256c5
ameralmunajjed <a.munajed@icloud.com>
2025-09-09 12:07:41 +0200

    about us it is ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
+
 	return (
-		<div>
-			<h1>Hello Recipe Book</h1>
+		<div className="flex items-start border-2 border-solid border-indigo-200 bg-sky-200 m-8 p-8 dark:bg-sky-800 dark:text-white">
+			<h1>src/app/Home </h1>
 		</div>
 	);
 }
