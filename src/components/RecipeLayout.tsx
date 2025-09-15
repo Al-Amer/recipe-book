3b541a6c8568c6fa5f8c3c3d58cb2d79eeca2538
grufre349 <grufre349@gmail.com>
2025-09-15 20:20:57 +0200

    added some styling to Overview Component, fixed error while fetching/destructuring data, need savepoint because of 'weird' behaviour in VSCode like no borders get displayed

--- src/components/RecipeLayout.tsx
+++ src/components/RecipeLayout.tsx
@@ -9,16 +9,16 @@ type PropSchema = {
 };
 
 export const RecipeLayout = ({ name, meals }: PropSchema) => {
-	console.log("log from RecipeLayout:", meals);
+	// console.log("log from RecipeLayout:", meals);
 	return (
 		// return a Layout for reusability in Landing and Search Page
 		// Pass in name as Headline
 
 		// ToDO
 		// Add a Layout and display recipes with ID from Array
-		<section className="border-2 flex flex-col items-center p-4">
+		<section className="border-2 rounded-2xl flex flex-col items-center p-4">
 			<h2>{name}</h2>
-			<div className="p-4">
+			<div>
 				{/* Use RecipeComponent to display a single recipe */}
 				{meals.map((meal) => (
 					<RecipeOverview key={meal} mealId={meal} />
--- src/components/RecipeOverview.tsx
+++ src/components/RecipeOverview.tsx
@@ -1,5 +1,6 @@
 // get Data with ID from DB and render Data accordingly
 import dbCon from "@/lib/dbCon";
+import Link from "next/link";
 
 type OverviewProps = {
 	mealId: number;
@@ -8,17 +9,60 @@ type OverviewProps = {
 const sql = dbCon();
 
 // make FN async and await for SQL Query
+// One query to rule them all XD
+// query meals table with mealsID
+// query other tables dynamically with results from query
 export const RecipeOverview = async ({ mealId }: OverviewProps) => {
-	const result = await sql`
-    SELECT * FROM meals AS m
-    WHERE m.id = ${mealId}
-    `;
-	// console.log(result);
-	const { name, id } = result[0];
+	// Declaring here because of scope
+	let meal_name, thumb, areas_name, categories_name;
+
+	// try-catch-blck because of random erros while destructuring -> for sure internet/db connection issues on my side
+	try {
+		const result = await sql`
+            SELECT
+                m.name AS meal_name,
+                m.thumb AS thumb,
+                c.name AS categories_name,
+                a.name AS areas_name
+            FROM meals AS m
+            LEFT JOIN categories AS c ON c.id = m.category_id
+            LEFT JOIN areas AS a ON a.id = m.area_id
+            WHERE m.id = ${mealId}
+        `;
+
+		// because i was getting random errors wich says result was undefined
+		const meal = result[0];
+		({ meal_name, thumb, areas_name, categories_name } = meal);
+	} catch (error) {
+		// Dieser Block fängt den "Cannot destructure... is undefined" Fehler ab
+		console.error("An error occured while working with the data:", error);
+		return (
+			<div className="border-2 m-2 p-4 rounded-2xl">
+				<h2>Error while loading data</h2>
+				<p>
+					The meal with the id of {mealId} could not be found or an
+					unexpected error occured, please try again.
+				</p>
+			</div>
+		);
+	}
 
 	return (
 		<div className="border-2 m-2 p-4 rounded-2xl">
-			<h3>{name}</h3>
+			<h3 className="p-4 text-center">{meal_name}</h3>
+
+			<div className="flex flex-col items-center">
+				<div className="border-red-500">
+					<img src={thumb} alt={meal_name} className="size-[50%]" />
+					<figcaption>{meal_name}</figcaption>
+				</div>
+				<div className="flex flex-col items-center">
+					<Link href={"/categories"}>
+						Category: {categories_name}
+					</Link>
+					<Link href={"/search"}>Area: {areas_name}</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
