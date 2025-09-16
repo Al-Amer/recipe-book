63b80e373bbc5cc65a3fb9f2d537cd857227826d
edubur <edu.bu@gmx.de>
2025-09-16 22:12:38 +0200

    adjusted RecipeOverview and header, since more links joined the navbar

--- src/components/RecipeOverview.tsx
+++ src/components/RecipeOverview.tsx
@@ -6,6 +6,11 @@ type OverviewProps = {
   mealId: number;
 };
 
+// make FN async and await for SQL Query
+// One query to rule them all XD
+// query meals table with mealsID
+// query other tables dynamically with results from query
+
 export const RecipeOverview = async ({ mealId }: OverviewProps) => {
   const sql = dbCon();
 
@@ -77,75 +82,3 @@ export const RecipeOverview = async ({ mealId }: OverviewProps) => {
     );
   }
 };
-
-
-
-
-
-
-
-
-
-// // get Data with ID from DB and render Data accordingly
-// import dbCon from "@/lib/dbCon";
-// import Link from "next/link";
-
-// type OverviewProps = {
-// 	mealId: number;
-// };
-
-// const sql = dbCon();
-
-// // make FN async and await for SQL Query
-// // One query to rule them all XD
-// // query meals table with mealsID
-// // query other tables dynamically with results from query
-// export const RecipeOverview = async ({ mealId }: OverviewProps) => {
-// 	// Declaring here because of scope
-// 	let meal_name, thumb, areas_name, categories_name;
-
-// 	// try-catch-blck because of random erros while destructuring -> for sure internet/db connection issues on my side
-	
-
-// 		// because i was getting random errors wich says result was undefined
-// 		const meal = result[0];
-// 		({ meal_name, thumb, areas_name, categories_name } = meal);
-// 	} catch (error) {
-// 		console.error("An error occured while working with the data:", error);
-// 		return (
-// 			<div className="border-2 m-2 p-4 rounded-2xl">
-// 				<h2>Error while loading data</h2>
-// 				<p>
-// 					The meal with the id of {mealId} could not be found or an
-// 					unexpected error occured, please try again.
-// 				</p>
-// 			</div>
-// 		);
-// 	}
-
-// 	// Mobie First Design/Development like suggested by Taiwind, add Design for Desktop later if functionality is
-// 	return (
-// 		<div className="border-2 m-2 p-4 rounded-2xl ">
-// 			<div className="flex flex-col items-center">
-// 				<Link href={`/categories/${mealId}`}>
-// 					<h3 className="p-4 text-center">{meal_name}</h3>
-// 					<div className="flex flex-col items-center">
-// 						<img
-// 							src={thumb}
-// 							alt={meal_name}
-// 							className="size-[50%]"
-// 						/>
-// 						<figcaption>{meal_name}</figcaption>
-// 					</div>
-// 				</Link>
-
-// 				<div className="flex flex-col items-center">
-// 					<Link href={"/categories"}>
-// 						Category: {categories_name}
-// 					</Link>
-// 					<Link href={"/search"}>Area: {areas_name}</Link>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
