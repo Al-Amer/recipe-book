e8279f246d170dbd88e89585c82c38c05eb7c803
edubur <edu.bu@gmx.de>
2025-09-17 14:52:08 +0200

    fixed import link in my-recipes api

--- src/app/my-recipes/page.tsx
+++ src/app/my-recipes/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route"
+import { authOptions } from "../../lib/authOptions"
 import {RecipeForm} from "../../components/RecipeForm";
 import {RecipesList} from "../../components/RecipesList";
 
